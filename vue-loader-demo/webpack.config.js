aa494cfbfb48cc51c8473ddb2d6dc73a2574418e
raoju <raoju1580@qq.com>
2017-09-13 10:10:26 +0800

    vue-loader + webpack 配置和其他依赖安装

--- vue-loader-demo/webpack.config.js
+++ vue-loader-demo/webpack.config.js
@@ -1,7 +1,20 @@
-module.exports={
-	entry:'./main.js',
-	output:{
-		path:__dirname,  //当前路径，，，使文件变成绝对路径
-		filename:'bulid.js'
-	}
+/*var webpack = require('webpack');
+var WebpackDevServer = require("webpack-dev-server");*/
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: __dirname, //当前路径，，，使文件变成绝对路径
+        filename: 'bulid.js'
+    },
+    module:{
+    	loaders:[
+    		{test:/\.vue$/,loader:'vue-loader'},//多个loader用感叹号链接   loader:'vue-loader ！css-loader'
+    		{test:/\.js$/,loader:'babel',exclude:/node_modules/}
+    	]
+    },
+    babel:{
+    	presets:['es2015'],
+    	plugins:['transform-runtime']
+    }
 }
