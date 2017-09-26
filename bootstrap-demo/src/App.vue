1237c342b9fe8159a569ea9b7aad1461645813b8
Jerome1580 <raoju1580@qq.com>
2017-09-27 01:01:32 +0800

    新增vue配合使用bootstrap 和element-ui组件

---
+++ bootstrap-demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    welcome
+    <button class="btn btn-default">普通按钮</button>
+    <button class="btn btn-danger">警告按钮</button>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
