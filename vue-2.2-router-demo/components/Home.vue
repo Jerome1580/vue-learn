6e9dbf3766792b4db060912f9aa3e9b9c40d2070
Jerome1580 <raoju1580@qq.com>
2017-11-22 23:11:23 +0800

    vue2.0-router路由补充，在组件中添加路由钩子

--- vue-2.2-router-demo/components/Home.vue
+++ vue-2.2-router-demo/components/Home.vue
@@ -3,3 +3,19 @@
 	   <h2>这是主页</h2>
 	</div>
 </template>
+<script>
+    export default {
+        // 路由的钩子也可以加在组件中
+        beforeRouteEnter:(to,from,next)=>{
+            console.log(to.path)
+            console.log(from.path)
+            next()
+        },
+        beforeRouteLeave:(to,from,next)=>{
+            console.log(to.path)
+            console.log(from.path)
+            next()
+        }
+
+    }
+</script>
