82fc62af0103d51315d02f399beec29c4bb4d20b
Jerome1580 <raoju1580@qq.com>
2017-11-22 23:10:55 +0800

    vue2.0-router路由补充，路由钩子

--- vue-2.2-router-demo/src/router.config.js
+++ vue-2.2-router-demo/src/router.config.js
@@ -3,10 +3,20 @@ import News from '../components/News.vue'
 import Transition from '../components/transition.vue'
 
 export default {
+    mode:'history',
+    base:__dirname,
 	routes:[
 		{path:'/home',component:Home},
         {path:'/news',component:News},
-		{path:'/transition',component:Transition},
+		{path:'/transition',component:Transition,
+            beforeEnter:(to,from,next)=>{
+                console.log(to)
+                console.log(from)
+                // next()  表示可以访问
+                // next(false) 或者是不写next() 表示不能访问
+                next()
+            }
+        },
 
 		{path:'/',component:Home},
 	]
